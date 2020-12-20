c3aa80a40560e60f1205a85a887aab2c502f75b6
dabogdan <39666410+dabogdan@users.noreply.github.com>
2020-12-20 21:42:00 +0200

    added ssu description into the overlay

--- js/overlay.js
+++ js/overlay.js
@@ -1,20 +1,23 @@
-const overlay = document.getElementById('overlay');
-const closeMenu = document.getElementById('close-menu');
 const descriptionPopup = document.getElementById('description-popup');
 const institutionText = document.getElementById('institution-text');
 const institutionTitle = document.getElementById('institution-title');
 const regionsPopup = document.getElementById('regions-popup');
+let openDescription = document.getElementsByClassName('open-description');
 // const regionsList = document.getElementById('regions-list');
 
 // insert the institution data and title into ".description-popup"
 let onClick = (element) => {
+    console.log(element);
     institutionTitle.innerHTML = element.getAttribute('description-title');
     institutionText.innerHTML = element.getAttribute('description-data');
 }
 
-document.querySelector('.open-description').addEventListener('click', () => {
-    descriptionPopup.classList.add('half-overlay');
-});
+//adding eventListeners to all tags with the class 'open-description'
+for(let i = 0; i < openDescription.length; i++) {
+    openDescription[i].addEventListener('click', () => {
+        descriptionPopup.classList.add('half-overlay');
+    });
+}
 document.getElementById('close-btn').addEventListener('click', () => {
     descriptionPopup.classList.remove('half-overlay');
 });
