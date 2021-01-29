32c18e69150f730c04d15be4c19718d3dfb7b562
dabogdan <39666410+dabogdan@users.noreply.github.com>
2021-01-29 18:31:44 +0200

    red shadow of the card logo

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -8,13 +8,15 @@ paragraphs.forEach((e) => {
 //change color of the card-image border
 let redBorderStyle = (e) => {
     e.children[0].children[0].style.border = '2px solid #EB1B51';
+    e.children[0].children[0].style.boxShadow = '2px 2px 10px rgba(200, 0, 0, 0.7)';
 }
 let normalBorderStyle = (e) => {
     e.children[0].children[0].style.border = '2px solid rgba(57,96,109,0)';
+    e.children[0].children[0].style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.8)';
 }
 
 let bigCardShadow = (e) => {
-    e.style.boxShadow = '2px 2px 25px rgba(0, 0, 0, 1)';
+    e.style.boxShadow = '2px 2px 25px rgb(0, 0, 0)';
 }
 
 let normalCardShadow = (e) => {
