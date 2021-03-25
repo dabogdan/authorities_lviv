00e014e3385a7b181cf1fccb60157338c44ea142
dabogdan <39666410+dabogdan@users.noreply.github.com>
2021-03-25 12:07:46 +0200

    empty placeholders when input is focused, stylisation of the about and support sections

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -1,7 +1,15 @@
 const paragraphs = document.querySelectorAll('.paragraph');
-const aboutBtn = document.querySelector('.about-link');
-const aboutSection = document.querySelector('#about');
 const header = document.querySelector('.header');
+const inputs = document.querySelectorAll('.input')
+
+const aboutLink = document.querySelectorAll('.about-link');
+const supportLink = document.querySelectorAll('.support-link')
+const aboutSection = document.querySelector('#about');
+const supportSection = document.querySelector('#support')
+const links = [aboutLink, supportLink];
+const anchors = [aboutSection, supportSection];
+
+console.log(links);
 
 //change of the logo border color to red on the cards when hover over them
 //add events to the button when card is highlighted
@@ -17,25 +25,37 @@ paragraphs.forEach((e) => {
 function highlightCard (e) {
     e.children[0].children[3].children[0].style.backgroundColor = 'white';
     e.style.boxShadow = 'inset 0 0 0 2000px rgba(234,254,0, 1)';
-    // e.style.filter = 'grayscale(0%)';
 }
 
 function unhighlightCard (e) {
     e.children[0].children[3].children[0].style.backgroundColor = '#EAFE00';
     e.style.boxShadow = 'inset 0 0 0 2000px rgba(238,238,238, 0.8)';
-    // e.style.filter = 'grayscale(100%)';
-    // e.children[0].children[1].filter = 'grayscale(0%)';
 }
 
 for (let i = 0; i < paragraphs.length; i++) {
     paragraphs[i].style.backgroundImage = institutions_Lviv_Region[i].photo;
 }
 
-// about link
-aboutBtn.addEventListener('click', () => {
-    aboutSection.style.display = 'block';
-})
+// events for links and anchors
+for (let i = 0; i < links.length; i++) {
+    for (let j = 0; j < links[i].length; j++) {
+        links[i][j].addEventListener('click', () => {
+            anchors[i].style.display = 'block';
+        })
+    }
 
+}
+
+//event listener for toggling the placeholder
+inputs.forEach((e) => {
+    e.originPlaceHolder = e.placeholder;
+    e.addEventListener('focus', () => {
+      e.placeholder = '';
+    });
+    e.addEventListener('blur', () => {
+        e.placeholder = e.originPlaceHolder;
+    })
+})
 
 //smooth-scroll with the offset of the header size
 if (screen.width > 480) {
